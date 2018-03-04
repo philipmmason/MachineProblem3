5f0bf8a9ef7dc92937bb7bc38cfadba0d29e5bd2
philipmmason <36638508+philipmmason@users.noreply.github.com>
2018-03-04 07:46:00 -0500

    Create publication.cpp

---
+++ publication.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "publication.h"
+
+using namespace std;
+
+class Publication
+{
+private:
+protected:
+public:
+  Publication(string t, double p)
+  {
+    string title = t;
+    price double = p;
+  };
+}
