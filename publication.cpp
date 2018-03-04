07386f546e2cc9c38bed0ba2469e0668553f786e
philipmmason <36638508+philipmmason@users.noreply.github.com>
2018-03-04 09:12:25 -0500

    Update publication.cpp

--- publication.cpp
+++ publication.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include "publication.h"
+#include "class.h"
 
 using namespace std;
 
+//Define class publication.
 Publication::Publication()
 {
 };
@@ -25,3 +26,26 @@ void displayData()
 {
   cout << "The title of this publication is " << title << " and it cost " << price << ".\n\n";
 };
+
+//Define derived class Books.
+class Book::Book()
+{
+};
+
+void Book::readData()
+{
+  cout << "Please enter name number of pages of the publication: \n\n";
+  cin >> pageCount;
+};
+
+//Define derived class Tape
+class Tape : public Publication
+{
+  private:
+  protected:
+  public:   
+      Tape(double time)
+      {
+        int time = time;
+      };
+ };
