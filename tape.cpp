40004f152ce9645c65daa1df8d247605e5413fd1
philipmmason <36638508+philipmmason@users.noreply.github.com>
2018-03-04 07:53:47 -0500

    Create tape.cpp

---
+++ tape.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "tape.h"
+
+using namespace std;
+
+class Tape : public Publication
+{
+  private:
+  protected:
+  public:   
+      Tape(double time)
+      {
+        int time = time;
+      };
+ };
