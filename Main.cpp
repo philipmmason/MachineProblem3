d877503da79c5f4485a4fe880015c3c889cc561b
philipmmason <36638508+philipmmason@users.noreply.github.com>
2018-03-10 11:54:22 -0500

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include "Class.h"
+
+using namespace std;
+
+int main()
+{
+	Book NewBook;
+	Tape NewTape;
+
+	NewBook.readData();
+	NewBook.displayData();
+
+	return 0;
+}
