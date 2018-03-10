88da90398ed435aff8beae719b9a9d33168b05ca
philipmmason <36638508+philipmmason@users.noreply.github.com>
2018-03-10 11:53:45 -0500

    Update class.cpp

--- class.cpp
+++ class.cpp
@@ -1,51 +1,24 @@
 #include <iostream>
-#include "class.h"
+#include <string>
+#include "Class.h"
 
 using namespace std;
 
-//Define class publication.
-Publication::Publication()
-{
-};
-
-void Publication::Publication()
-{
-  string title;
-  price double;
-};
-
-void Publication::readData()
-{
-  cout << "Please enter the the title of the publication: " << endl;
-  cin >> title >> endl;
-  cout << "Please enter the price of this publication: " << endl;
-  cin >> price >> endl;
-};
-
-void displayData()
-{
-  cout << "The title of this publication is " << title << " and it cost " << price << ".\n\n";
-};
-
-//Define derived class Books.
-class Book::Book()
-{
-};
 
 void Book::readData()
 {
-  cout << "Please enter name number of pages of the publication: \n\n";
-  cin >> pageCount;
-};
+	cout << "Please enter the title of the book: "; getline(cin, title);
+}
 
-//Define derived class Tape
-class Tape : public Publication
+void Book::displayData()
 {
-  private:
-  protected:
-  public:   
-      Tape(double time)
-      {
-        int time = time;
-      };
- };
+	cout << title;
+}
+
+void Tape::readData()
+{
+}
+
+void Tape::displayData()
+{
+}
