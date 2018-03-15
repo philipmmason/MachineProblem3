f21f67d57f7d02f03c90933efef2f05a245aa502
philipmmason <36638508+philipmmason@users.noreply.github.com>
2018-03-15 10:37:58 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#inclide "Class.h"
+
+using namespace std;
+
+int main()
+{
+  Book Book1;//Book object
+  Book *ptrBook;//pointer of class Book
+  ptrBook = &Book1;//Give address of object Book1 to Book class pointer
+  
+  Tape Tape1;//Tape Object
+  Tape *ptrTape;//Pointer of class Tape
+  ptrTape = &Tape;//Give address of object Tape to Tape1 class pointer
+
+  cout << "Please enter the : " << ptrBox->Volume() << endl;
+
+
+}
