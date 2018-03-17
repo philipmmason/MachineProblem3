3c697f6dbabe23cfd435b0c7f281508007ff9d39
philipmmason <36638508+philipmmason@users.noreply.github.com>
2018-03-17 19:48:56 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 #include <string>
-#inclide "Class.h"
+#include "Class.h"
 
-using namespace std;
+; using namespace std;
 
 int main()
 {
-  Book Book1;//Book object
-  Book *ptrBook;//pointer of class Book
-  ptrBook = &Book1;//Give address of object Book1 to Book class pointer
-  
-  Tape Tape1;//Tape Object
-  Tape *ptrTape;//Pointer of class Tape
-  ptrTape = &Tape;//Give address of object Tape to Tape1 class pointer
-  
-  ptrBook->readData();//using class pointer to access member function
-  ptrBook->diplayData();//using class pointer to access member finction
- 
+	Book Book1;//Book object
+	Book *ptrBook1;//pointer of class Book
+	ptrBook1 = &Book1;//Give address of object Book1 to Book class pointer
 
+	Tape Tape1;//Tape Object
+	Tape *ptrTape1;//Pointer of class Tape
+	ptrTape1 = &Tape1;//Give address of object Tape to Tape1 class pointer
+
+
+	
+	Book1.readData();//user data for book
+	cin.ignore();
+	Tape1.readData();//user data for tape
+
+	system("CLS");//clear screen
+
+	ptrBook1->displayData();//using class pointer to access member finction
+	ptrTape1->displayData();//using class pointer to access member finction
+
+	return 0;
 }
