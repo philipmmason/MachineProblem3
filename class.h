7a93b2d1410de4af7dd8cd91d82b478148c65772
philipmmason <36638508+philipmmason@users.noreply.github.com>
2018-03-18 08:05:49 -0400

    Update class.h

--- class.h
+++ class.h
@@ -21,7 +21,7 @@ public:
 	void displayData();
 };
 
-class Book : public Publication
+class Book : public Publication : public Sales
 {
 private:
 protected:
@@ -31,7 +31,7 @@ public:
 	void displayData();
 };
 
-class Tape : public Publication
+class Tape : public Publication : public Sales
 {
 private:
 protected:
@@ -47,18 +47,8 @@ private:
 protected:
 public:
 	double sales[3] {};
-	void readData()
-	{
-		cout << "Please enter the sales over the last three months of this publication." << endl;
-		cout << "First Month: "; cin >> sales[0]; 
-		cout << "\nSecond Month: "; cin >> sales[1]; 
-		cout << "\nThird Month: "; cin >> sales[2]; 
-	};
-	
+	void readData()	
 	void displayData();
 	
 }
 #endif
-Now create another base class called sales; that holds an array of the three doubles so that you can record the dollar sales of a 
-particular publication (books or tapes) for the last 3 months (use any numbers you wish). Include a readData() function to get 3 
-sales amounts from the user, and a displayData() function to display the sales figures.
